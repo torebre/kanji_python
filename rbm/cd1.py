d788538ea2bfc7f13fc88115308fd1776a2f5c41
Tore Brede <tore.brede@gmail.com>
2018-02-11 23:00:36 +0100

    Working on getting RBM to work

--- rbm/cd1.py
+++ rbm/cd1.py
@@ -2,24 +2,18 @@ import tensorflow as tf
 
 # This is the gradient function
 def cd1(inputs, rbm_weights):
-    # TODO It seems strange to sample from a random uniform distribution, and then see how many inputs are greater than the sampled values. Is this correct?
-    rand_visible = tf.random_uniform(inputs.shape)
-    # visible_data_sampled = tf.reshape(tf.cast(tf.greater_equal(inputs, rand_visible), "float"), inputs.shape)
-    visible_data_sampled = tf.cast(tf.greater_equal(inputs, rand_visible), "float32")
+    visible_data_sampled = tf.cast(tf.greater_equal(inputs, tf.random_uniform(inputs.shape)), "float32")
 
     # visible_data = sample_bernoulli(visible_data);
 
     # visible_data_shape = [visible_data.shape[0].value, 1]
     # hidden_states_shape = [rbm_weights.shape[0].value, 1]
 
-
-    shapeWeightsNumberOfConfigurations = inputs.shape
-
-    divide_term = tf.constant(1., "float32", [rbm_weights.shape[0], visible_data_sampled.shape[1]])
+    # Visible state to hidden probabilities
+    ones = tf.constant(1, "float32", [rbm_weights.shape[0], visible_data_sampled.shape[1]])
     multiply_term = tf.exp(tf.matmul(tf.negative(rbm_weights), visible_data_sampled))
-    ones = tf.constant(1., "float32", [rbm_weights.shape[0], visible_data_sampled.shape[1]])
 
-    hidden_probabilities = tf.div(divide_term, tf.add(ones, multiply_term))
+    hidden_probabilities = tf.div(ones, tf.add(ones, multiply_term))
 
     # hidden_probability = 1. / (1 + exp(-rbm_w * visible_state));
 
@@ -29,7 +23,8 @@ def cd1(inputs, rbm_weights):
 
     # hidden_states = sample_bernoulli(hidden_probabilities);
 
-    ones_hidden = tf.constant(1., "float32", inputs.shape)
+    # Compute the hidden state to visible probabilities
+    ones_hidden = tf.constant(1, "float32", inputs.shape)
     multiply_term_hidden = tf.exp(tf.matmul(tf.transpose(tf.negative(rbm_weights)), hidden_states))
     visible_probabilities = tf.div(ones_hidden, tf.add(ones_hidden, multiply_term_hidden))
 
@@ -37,7 +32,7 @@ def cd1(inputs, rbm_weights):
 
     # visible_probabilities = hidden_state_to_visible_probabilities(rbm_w, hidden_states);
 
-    visible_states = tf.reshape(tf.cast(tf.greater_equal(visible_probabilities, tf.reshape(rand_visible, inputs.shape)), "float32"), inputs.shape)
+    visible_states = tf.cast(tf.greater_equal(visible_probabilities, tf.random_uniform(inputs.shape)), "float32")
 
     # visible_states = sample_bernoulli(visible_probabilities);|
 
@@ -49,14 +44,15 @@ def cd1(inputs, rbm_weights):
 
     # hidden_probabilities2 = visible_state_to_hidden_probabilities(rbm_w, visible_states);
 
-    hidden_states2 =tf.reshape(tf.cast(tf.greater_equal(hidden_probabilities2,  tf.random_uniform(hidden_probabilities.shape)), "float32"), hidden_states.shape)
+    # hidden_states2 = tf.cast(tf.greater_equal(hidden_probabilities2,  tf.random_uniform(hidden_probabilities.shape)), "float32")
 
     # hidden_states2 = sample_bernoulli(hidden_probabilities2);
 
-    visible_transposed = tf.transpose(tf.reshape(inputs, inputs.shape))
-    divide_by = tf.constant(inputs.shape[1].value, dtype = "float32")
-    term1 = tf.div(tf.matmul(hidden_states, visible_transposed), divide_by)
+    numberOfConfigurations = tf.constant(inputs.shape[1].value, dtype = "float32")
+
+    term1 = tf.div(tf.matmul(hidden_states, tf.transpose(inputs)), numberOfConfigurations)
     term2 = tf.div(tf.matmul(hidden_probabilities2, tf.transpose(visible_states)), hidden_probabilities2.shape[1].value)
+
     return tf.subtract(term1, term2)
 
 
--- rbm/rbmTest.py
+++ rbm/rbmTest.py
@@ -2,70 +2,58 @@ import numpy as np
 import scipy.io as sio
 import tensorflow as tf
 from cd1 import cd1
+from showRbm import showWeights
 
 dataSet = sio.loadmat("/home/student/Documents/Neural networks for Machine Learning/assignment4/data_set.mat")
 
+learningRate = 0.9
+momentumSpeedConstant = 0.9
+miniBatchSize = 100
+numberOfHiddenUnits = 50
 
 inputs = dataSet["data"]["training"][0, 0]["inputs"][0, 0].astype("float32")
+# inputs = np.reshape(dataSet["data"]["training"][0, 0]["inputs"][0, 0].astype("float32")[:, 0:2], (256, 2))
+
 targets = dataSet["data"]["training"][0, 0]["targets"][0, 0].astype("float32")
 
-numberOfHiddenUnits = 50
-rbmWeights = np.random.random_sample((numberOfHiddenUnits, inputs.shape[0])).astype("float32")
+inputsConstant = tf.placeholder(dtype="float32", shape=inputs.shape, name="inputs")
+weights = tf.Variable(initial_value=np.random.random_sample((numberOfHiddenUnits, inputs.shape[0])).astype("float32"),
+                      name="weights")
 
+weightSummary = tf.summary.image('input', tf.reshape(weights[0, :], [-1, 16, 16, 1]), 1)
+writer = tf.summary.FileWriter("../model_output/logs2")
 
-inputsConstant = tf.placeholder(dtype="float32", shape = inputs.shape, name="inputs")
-weights = tf.Variable(initial_value=rbmWeights, name="weights")
-
-# outputTensor = cd1(inputs, rbmWeights)
-
-
-# weightSummary = tf.summary.image("weights", weights)
-weightSummary = tf.summary.image('input', tf.reshape(weights, [-1, 16, 16, 1]), 1)
-
-writer = tf.summary.FileWriter("./logs")
-
-learningRate = 0.9
-
-momentumSpeed = tf.Variable(np.random.random_sample(weights.shape).astype("float32"), dtype="float32")
-miniBatchSize = 100
+momentumSpeed = tf.Variable(np.zeros(weights.shape).astype("float32"), dtype="float32")
 startOfNextMiniBatch = 0
 
-
-
-miniBatch = tf.placeholder(dtype="float32", shape = [inputs.shape[0], miniBatchSize], name="miniBatch")
+miniBatch = tf.placeholder(dtype="float32", shape=[inputs.shape[0], miniBatchSize], name="miniBatch")
 gradient = cd1(miniBatch, weights)
-# gradient = tf.placeholder(dtype="float32", shape = rbmWeights.shape, name="gradient")
 
-momentumSpeedUpdate = tf.add(tf.multiply(tf.constant(0.9, dtype="float32"), momentumSpeed), gradient)
-modelUpdate = tf.add(rbmWeights, tf.multiply(momentumSpeedUpdate, tf.constant(0.9, dtype="float32")))
+momentumSpeedUpdated = tf.add(tf.multiply(tf.constant(momentumSpeedConstant, dtype="float32"), momentumSpeed), gradient)
+weightsUpdated = tf.add(weights, tf.multiply(momentumSpeedUpdated, tf.constant(learningRate, dtype="float32")))
 
 with tf.Session() as sess:
     momentumSpeed.initializer.run()
     weights.initializer.run()
 
-    for i in range(10000):
+    for i in range(1000):
         miniBatchValues = inputs[:, startOfNextMiniBatch: (startOfNextMiniBatch + miniBatchSize)]
-
         startOfNextMiniBatch = np.mod(startOfNextMiniBatch + miniBatchSize, inputs.shape[1])
 
-        # gradient = sess.run(gradientComputation, feed_dict={miniBatch: miniBatchValues})
-        # sess.run([gradient, momentumSpeedUpdate], feed_dict={miniBatch: miniBatchValues})
-        momentumSpeed, weights= sess.run([momentumSpeedUpdate, modelUpdate], feed_dict={miniBatch: miniBatchValues})
+        updatedSpeed, updatedWeights = sess.run([momentumSpeedUpdated, weightsUpdated], feed_dict={miniBatch: miniBatchValues})
 
-        print("weights sample: ", weights[10, 10])
+        updateMomentumSpeed = momentumSpeed.assign(updatedSpeed)
+        updateWeights = weights.assign(updatedWeights)
+
+        sess.run([updateMomentumSpeed, updateWeights])
+
+        # print("i: ", i)
 
         summary_str = sess.run(weightSummary)
 
-        # momentumSpeed = sess.run(momentumSpeedUpdate)
-        # weights = learnedWeights
-
-        # _, _, summary_str = sess.run([momentumSpeedUpdate, modelUpdate, weightSummary], feed_dict={miniBatch: miniBatchValues})
-
         writer.add_summary(summary_str, i)
 
-        # momentumSpeed = 0.9 * momentumSpeed + gradient
-        # model = model + momentumSpeed * learningRate
 
-        # print("i: ", i, "weights: ", weights)
-
+    finalWeights = sess.run(weights)
+    showWeights(finalWeights)
 
